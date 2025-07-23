1abf78d3c0519aae7bc7f17707b16de63ef71791
Edwinmwangi-44paid <edumwas10254@gmail.com>
2025-07-23 17:29:03 +0300

    third commit

--- application.py
+++ application.py
@@ -2,12 +2,32 @@ from flask import Flask
 
 app = Flask(__name__)
 
+from flask_sqlalchemy import SQLAlchemy
+
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+db = SQLAlchemy(app)
+
+class Drink (db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    description = db.Column(db.String(200), nullable=True)
+
+
+    def __repr__(self):
+        return f'{self.name} - {self.description}'
 
 @app.route('/')
 def index():
-    return "Hello, World!"
+    return "Hello  wrld!"
 
-@app.route('/drinks')
+@app.route('/drink')
 def get_drinks():
-    
-    return "Here are your drinks!"
+    drinks = Drink.query.all()
+    output = []
+    for drink in drinks:
+        drink_data = { 'name': drink.name, 'description': drink.description}
+        output.append(drink_data)
+
+    return {"drinks": output}
